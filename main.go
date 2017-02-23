36e70a326389bd5efb8ed9790b2bade3b431ce0e
zeucxb <zeucxb@gmail.com>
2017-02-22 21:45:54 -0300

    add cli to organize the application

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "bbb-future/crowler"
+import (
+	"bbb-future/crowler"
+	"fmt"
+	"os"
+
+	cli "gopkg.in/urfave/cli.v2"
+)
 
 func main() {
-	crowler.SaveBrothersData()
+	app := &cli.App{
+		Name: "bbb-future",
+		Commands: []cli.Command{
+			{
+				Name:        "run",
+				Aliases:     []string{"r"},
+				Usage:       "run the server",
+				Description: "This start the server application",
+				Action: func(c *cli.Context) error {
+					fmt.Printf("TODO")
+					return nil
+				},
+			}, {
+				Name:        "crowler",
+				Aliases:     []string{"c"},
+				Usage:       "run the crowler and populate db",
+				Description: "This run a spider and populate the database",
+				Action: func(c *cli.Context) error {
+					crowler.SaveBrothersData()
+					return nil
+				},
+			},
+		},
+	}
+
+	app.Run(os.Args)
 }
